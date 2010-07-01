71339fe51696813f52b7b9eb6ed9e5d534af3c78
Chris Hulbert <chris.hulbert@gmail.com>
2010-07-01 16:40:31 +1000

    AES implemented

---
+++ c/c_aes.bat
@@ -0,0 +1,11 @@
+@echo off
+rem You may want to update the paths below depending on your version of VS
+set path=%path%;C:\Program Files\Microsoft Visual Studio 8\VC\bin
+set path=%path%;C:\Program Files\Microsoft Visual Studio 8\Common7\IDE
+set lib=%lib%;C:\Program Files\Microsoft Visual Studio 8\VC\lib
+set include=%include%;C:\Program Files\Microsoft Visual Studio 8\VC\include
+
+cl c_aes.c
+del c_aes.obj
+c_aes.exe
+pause
