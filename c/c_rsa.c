7da5fd3d225d38f22b1358e26964d44e1f9956b3
Chris Hulbert <chris.hulbert@gmail.com>
2010-07-13 16:47:23 +1000

    RSA in C

--- c/c_rsa.c
+++ c/c_rsa.c
@@ -18,6 +18,7 @@ char *e_str = "010001";
 char *n_str = "c238d450c526bb2014b1489505540eb8330c7e01ed7ac4a7d9a52423025f9bdd5eb42b2103b6a069e43678bef68fa67703c304c590c6629bd455f4d8c0a145599df37bdefa19b52532937a2ccc22fb36f73c6dad819bc01e1326028fab37a052e0efae05e437573f2254a5ea4a43d1f3dbec2b22bf24fc6dddd0443f6ebda957";
 char *d_str = "305bf211826558666e808deffcf9a7089a3d5c0aa2d4d4ae6e74be00b19098c08fda107b11efa1157cab4b7950ef07a5ce9bfa4e2ef4168d725b4cb1c394e42d332999fa20a42f4c31fdeba079c6931a11915f66d2b47c75571d334ce075bc417df8bc0848ae97b7abf6472ab7c83de2da691115a864d32496200d26a1d91791";
 char *message = "This is going to be embarrasing if it doesn't work!";
+char *m_str = "5468697320697320676f696e6720746f20626520656d626172726173696e6720696620697420646f65736e277420776f726b21";
 char *c_str = "68c1a28435c90c20e3e0111302f97222c875215ce37178cdca30fbd90fceafaa7aa90c5d0dee2290a3b4cf944a177175acd5cb29cb03869bce2b4f93357cb94b08f8f1f08f793f9a7015338be19ff6b9301aa144665ffe0f7749885d3c3a51f8627d1e26ad629525eee59da7d5c69fe2926b6fb51ded336b6033a203d1ef5bc3";
 
 // Some bignum functions
@@ -167,18 +168,43 @@ void bignum_mod(bignum *a, bignum *b, bignum *out) {
   }
 }
 
+// Returns nonzero if a>0
+int bignum_gzero(bignum *a) {
+  for (int i=0;i<BIGNUM_MAX_BYTES;i++) {
+    if (a->bytes[i]!=0) return 1;
+  }
+  return 0;
+}
+
+// Do base ^ power % mod
+void bignum_modpow(bignum *base, bignum *power, bignum *mod, bignum *out) {
+ bignum temp;
+ string_to_bignum("01", out); // result = 1
+ while(bignum_gzero(power)) { // while power > 0
+   if (power->bytes[0]&1) {
+     bignum_mult(out,base,&temp); // temp = result*base
+     bignum_mod(&temp,mod,out); // result = temp % mod
+   }
+   bignum_mult(base,base,&temp); // temp = base*base
+   bignum_mod(&temp,mod,base); // base = temp % mod
+   bignum_shiftright_onebit(power); // power>>=1
+ }
+}
+
 // Test it
 int main(void) {
-  bignum p, q, n;
+  bignum p, q, n, m, e;
   string_to_bignum(p_str, &p);
   string_to_bignum(q_str, &q);
   string_to_bignum(n_str, &n);
+  string_to_bignum(m_str, &m);
+  string_to_bignum(e_str, &e);
   print_bignum(&p, "P prime");
   print_bignum(&q, "Q prime");
   print_bignum(&n, "N modulus");
   
   // Test %
-  bignum mod;
-  bignum_mod(&n, &p, &mod);
-  print_bignum(&mod, "N % P");
+  bignum c;
+  bignum_modpow(&m, &e, &n, &c); // Encrypt: c = m^e mod n
+  print_bignum(&c, "C");
 }
