86a168afd492bcea6aadd40779ac22a31cde9859
Douglas Stebila <stebilad@mcmaster.ca>
2018-09-05 21:53:06 -0400

    Add AES-256

--- c/c_aes.c
+++ c/c_aes.c
@@ -1,4 +1,4 @@
-// Simple, thoroughly commented implementation of 128-bit AES / Rijndael using C
+// Simple, thoroughly commented implementation of 128-bit and 256-bit AES / Rijndael using C
 // Chris Hulbert - chris.hulbert@gmail.com - http://splinter.com.au/blog
 // References:
 // http://en.wikipedia.org/wiki/Advanced_Encryption_Standard
@@ -8,6 +8,7 @@
 // This code is public domain, or any OSI-approved license, your choice. No warranty.
 
 #include <stdio.h>
+#include <stdint.h>
 #include <string.h>
 
 typedef unsigned char byte;
@@ -191,6 +192,15 @@ static void sub_bytes_inv(byte *a,int n) {
     }
 }
 
+// Rotate the first four bytes of the input eight bits to the left
+static inline void rot_word(byte *a) {
+    byte temp = a[0];
+    a[0]=a[1];
+    a[1]=a[2];
+    a[2]=a[3];
+    a[3]=temp;
+}
+
 // Perform the core key schedule transform on 4 bytes, as part of the key expansion process
 // http://en.wikipedia.org/wiki/Rijndael_key_schedule#Key_schedule_core
 static void key_schedule_core(byte *a, int i) {
@@ -206,7 +216,7 @@ static void key_schedule_core(byte *a, int i) {
 
 // Expand the 16-byte key to 11 round keys (176 bytes)
 // http://en.wikipedia.org/wiki/Rijndael_key_schedule#The_key_schedule
-static void expand_key(const byte *key, byte *keys) {
+static void expand_key128(const byte *key, byte *keys) {
     int bytes=16;             // The count of how many bytes we've created so far
     int i=1;                  // The rcon iteration value i is set to 1
     int j;                    // For repeating the second stage 3 times
@@ -227,6 +237,37 @@ static void expand_key(const byte *key, byte *keys) {
             xor(t,keys+bytes-16,4);            // We exclusive-or t with the four-byte block n bytes before
             memcpy(keys+bytes,t,4);            // This becomes the next 4 bytes in the expanded key
             bytes+=4;                          // Keep track of how many expanded key bytes we've added
+        }
+    }
+}
+
+// Expand the 32-byte key to 15 round keys (240 bytes)
+// http://en.wikipedia.org/wiki/Rijndael_key_schedule#The_key_schedule
+static void expand_key256(const byte *key, byte *keys) {
+    int i=0;                                      // The count of how many iterations we've done
+    byte t[4];                                    // Temporary working area
+
+    // The first 32 bytes of the expanded key are simply the encryption key
+    memcpy(keys, key, 8 * 4);
+
+    // The remaining 240-32 bytes of the expanded key are computed in one of three ways:
+    for (i = 8; i < 4 * 15; i++) {
+        if (i % 8 == 0) {
+            memcpy(t, keys + 4 * (i - 1), 4);     // We assign the value of the previous 4 bytes in the expanded key to t
+            sub_bytes(t, 4);                      // We apply byte-wise substitution to t
+            rot_word(t);                          // We rotate t one byte left
+            t[0] ^= lookup_rcon[i / 8];           // We xor in the round constant
+            xor(t, keys + 4 * (i - 8), 4);        // We xor in the four-byte block n bytes before
+            memcpy(keys + 4 * i, t, 4);           // This becomes the next 4 bytes in the expanded key
+        } else if (i % 8 == 4) {
+            memcpy(t, keys + 4 * (i - 1), 4);     // We assign the value of the previous 4 bytes in the expanded key to t
+            sub_bytes(t, 4);                      // We apply byte-wise substitution to t
+            xor(t, keys + 4 * (i - 8), 4);        // We xor in the four-byte block n bytes before
+            memcpy(keys + 4 * i, t, 4);           // This becomes the next 4 bytes in the expanded key
+        } else {
+            memcpy(t, keys + 4 * (i - 1), 4);     // We assign the value of the previous 4 bytes in the expanded key to t
+            xor(t, keys + 4 * (i - 8), 4);        // We xor in the four-byte block n bytes before
+            memcpy(keys + 4 * i, t, 4);           // This becomes the next 4 bytes in the expanded key
         }
     }
 }
@@ -294,12 +335,12 @@ static void mix_cols_inv (byte *state) {
 
 // Encrypt a single 128 bit block by a 128 bit key using AES
 // http://en.wikipedia.org/wiki/Advanced_Encryption_Standard
-static void EncryptAES(const byte *msg, const byte *key, byte *c) {
+static void EncryptAES128(const byte *msg, const byte *key, byte *c) {
     int i; // To count the rounds
 
     // Key expansion
     byte keys[176];
-    expand_key(key,keys);
+    expand_key128(key,keys);
 
     // First Round
     memcpy(c, msg, 16);
@@ -319,14 +360,41 @@ static void EncryptAES(const byte *msg, const byte *key, byte *c) {
     xor_round_key(c, keys, 10);
 }
 
+// Encrypt a single 128 bit block by a 256 bit key using AES
+// http://en.wikipedia.org/wiki/Advanced_Encryption_Standard
+static void EncryptAES256(const byte *msg, const byte *key, byte *c) {
+    int i; // To count the rounds
+
+    // Key expansion
+    byte keys[240];
+    expand_key256(key,keys);
+
+    // First Round
+    memcpy(c, msg, 16);
+    xor_round_key(c,keys,0);
+
+    // Middle rounds
+    for(i=0; i<13; i++) {
+        sub_bytes(c,16);
+        shift_rows(c);
+        mix_cols(c);
+        xor_round_key(c, keys, i+1);
+    }
+
+    // Final Round
+    sub_bytes(c,16);
+    shift_rows(c);
+    xor_round_key(c, keys, 14);
+}
+
 // Decrypt a single 128 bit block by a 128 bit key using AES
 // http://en.wikipedia.org/wiki/Advanced_Encryption_Standard
-static void DecryptAES(const byte *c, const byte *key, byte *m) {
+static void DecryptAES128(const byte *c, const byte *key, byte *m) {
     int i; // To count the rounds
 
     // Key expansion
     byte keys[176];
-    expand_key(key,keys);
+    expand_key128(key,keys);
 
     // Reverse the final Round
     memcpy(m,c,16);
@@ -346,33 +414,172 @@ static void DecryptAES(const byte *c, const byte *key, byte *m) {
     xor_round_key(m, keys, 0);
 }
 
-// Pretty-print a key (or any smallish buffer) onto screen as hex
-static void Pretty(const byte* b,int len,char* label) {
-    char out[100];
-    int i;
-    for (i=0; i<len; i++) {
-        sprintf(out+i*2,"%02x",b[i]);
+// Decrypt a single 128 bit block by a 256 bit key using AES
+// http://en.wikipedia.org/wiki/Advanced_Encryption_Standard
+static void DecryptAES256(const byte *c, const byte *key, byte *m) {
+    int i; // To count the rounds
+
+    // Key expansion
+    byte keys[240];
+    expand_key256(key,keys);
+
+    // Reverse the final Round
+    memcpy(m,c,16);
+    xor_round_key(m,keys,14);
+    shift_rows_inv(m);
+    sub_bytes_inv(m, 16);
+
+    // Reverse the middle rounds
+    for (i=0; i<13; i++) {
+        xor_round_key(m,keys,13-i);
+        mix_cols_inv(m);
+        shift_rows_inv(m);
+        sub_bytes_inv(m, 16);
     }
 
-    printf("%s%s\r\n",label, out);
+    // Reverse the first Round
+    xor_round_key(m, keys, 0);
+}
+
+// Pretty-print a key (or any smallish buffer) onto screen as hex
+static void Pretty(const byte* b,int len,char* label) {
+    printf("%s", label);
+    int i;
+    for (i=0; i<len; i++) {
+        printf("%02x", b[i]);
+    }
+    printf("\n");
+}
+
+int test_expand_key_128() {
+    // test vector from Appendix A.1 of FIPS 197
+    byte key[] = {0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c};
+    byte expected_keys[] = {0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c, 0xa0, 0xfa, 0xfe, 0x17, 0x88, 0x54, 0x2c, 0xb1, 0x23, 0xa3, 0x39, 0x39, 0x2a, 0x6c, 0x76, 0x05, 0xf2, 0xc2, 0x95, 0xf2, 0x7a, 0x96, 0xb9, 0x43, 0x59, 0x35, 0x80, 0x7a, 0x73, 0x59, 0xf6, 0x7f, 0x3d, 0x80, 0x47, 0x7d, 0x47, 0x16, 0xfe, 0x3e, 0x1e, 0x23, 0x7e, 0x44, 0x6d, 0x7a, 0x88, 0x3b, 0xef, 0x44, 0xa5, 0x41, 0xa8, 0x52, 0x5b, 0x7f, 0xb6, 0x71, 0x25, 0x3b, 0xdb, 0x0b, 0xad, 0x00, 0xd4, 0xd1, 0xc6, 0xf8, 0x7c, 0x83, 0x9d, 0x87, 0xca, 0xf2, 0xb8, 0xbc, 0x11, 0xf9, 0x15, 0xbc, 0x6d, 0x88, 0xa3, 0x7a, 0x11, 0x0b, 0x3e, 0xfd, 0xdb, 0xf9, 0x86, 0x41, 0xca, 0x00, 0x93, 0xfd, 0x4e, 0x54, 0xf7, 0x0e, 0x5f, 0x5f, 0xc9, 0xf3, 0x84, 0xa6, 0x4f, 0xb2, 0x4e, 0xa6, 0xdc, 0x4f, 0xea, 0xd2, 0x73, 0x21, 0xb5, 0x8d, 0xba, 0xd2, 0x31, 0x2b, 0xf5, 0x60, 0x7f, 0x8d, 0x29, 0x2f, 0xac, 0x77, 0x66, 0xf3, 0x19, 0xfa, 0xdc, 0x21, 0x28, 0xd1, 0x29, 0x41, 0x57, 0x5c, 0x00, 0x6e, 0xd0, 0x14, 0xf9, 0xa8, 0xc9, 0xee, 0x25, 0x89, 0xe1, 0x3f, 0x0c, 0xc8, 0xb6, 0x63, 0x0c, 0xa6};
+    byte keys[176];
+
+    printf("\nTest AES128 key expansion\n");
+    expand_key128(key, keys);
+    if (memcmp(keys, expected_keys, 176) == 0) {
+        printf("Expanded key matches expected value.\n");
+        return 0;
+    } else {
+        printf("Expanded key does not match expected value!\n");
+        Pretty(expected_keys, 176, "Expected expanded key: ");
+        Pretty(keys, 176, "Actual expanded key:   ");
+        return -1;
+    }
+}
+
+int test_encrypt_128() {
+    // test vector from Appendix C.1 of FIPS 197
+    byte plaintext[] = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff};
+    byte key[] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f};
+    byte expected_ciphertext[] = {0x69, 0xc4, 0xe0, 0xd8, 0x6a, 0x7b, 0x04, 0x30, 0xd8, 0xcd, 0xb7, 0x80, 0x70, 0xb4, 0xc5, 0x5a};
+    byte ciphertext[16];
+
+    printf("\nTest AES128 encryption\n");
+    EncryptAES128(plaintext, key, ciphertext);
+    if (memcmp(ciphertext, expected_ciphertext, 16) == 0) {
+        printf("Ciphertext matches expected value.\n");
+        return 0;
+    } else {
+        printf("Ciphertext does not match expected value!\n");
+        Pretty(expected_ciphertext, 16, "Expected ciphertext: ");
+        Pretty(ciphertext, 16, "Actual ciphertext:   ");
+        return -2;
+    }
+}
+
+int test_decrypt_128() {
+    // test vector from Appendix C.1 of FIPS 197
+    byte ciphertext[] = {0x69, 0xc4, 0xe0, 0xd8, 0x6a, 0x7b, 0x04, 0x30, 0xd8, 0xcd, 0xb7, 0x80, 0x70, 0xb4, 0xc5, 0x5a};
+    byte expected_plaintext[] = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff};
+    byte key[] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f};
+    byte plaintext[16];
+
+    printf("\nTest AES128 decryption\n");
+    DecryptAES128(ciphertext, key, plaintext);
+    if (memcmp(plaintext, expected_plaintext, 16) == 0) {
+        printf("Plaintext matches expected value.\n");
+        return 0;
+    } else {
+        printf("Plaintext does not match expected value!\n");
+        Pretty(expected_plaintext, 16, "Expected plaintext: ");
+        Pretty(plaintext, 16, "Actual plaintext:   ");
+        return -4;
+    }
+}
+
+int test_expand_key_256() {
+    // test vector from Appendix A.3 of FIPS 197
+    byte key[] = {0x60, 0x3d, 0xeb, 0x10, 0x15, 0xca, 0x71, 0xbe, 0x2b, 0x73, 0xae, 0xf0, 0x85, 0x7d, 0x77, 0x81, 0x1f, 0x35, 0x2c, 0x07, 0x3b, 0x61, 0x08, 0xd7, 0x2d, 0x98, 0x10, 0xa3, 0x09, 0x14, 0xdf, 0xf4};
+    byte expected_keys[] = {0x60, 0x3d, 0xeb, 0x10, 0x15, 0xca, 0x71, 0xbe, 0x2b, 0x73, 0xae, 0xf0, 0x85, 0x7d, 0x77, 0x81, 0x1f, 0x35, 0x2c, 0x07, 0x3b, 0x61, 0x08, 0xd7, 0x2d, 0x98, 0x10, 0xa3, 0x09, 0x14, 0xdf, 0xf4, 0x9b, 0xa3, 0x54, 0x11, 0x8e, 0x69, 0x25, 0xaf, 0xa5, 0x1a, 0x8b, 0x5f, 0x20, 0x67, 0xfc, 0xde, 0xa8, 0xb0, 0x9c, 0x1a, 0x93, 0xd1, 0x94, 0xcd, 0xbe, 0x49, 0x84, 0x6e, 0xb7, 0x5d, 0x5b, 0x9a, 0xd5, 0x9a, 0xec, 0xb8, 0x5b, 0xf3, 0xc9, 0x17, 0xfe, 0xe9, 0x42, 0x48, 0xde, 0x8e, 0xbe, 0x96, 0xb5, 0xa9, 0x32, 0x8a, 0x26, 0x78, 0xa6, 0x47, 0x98, 0x31, 0x22, 0x29, 0x2f, 0x6c, 0x79, 0xb3, 0x81, 0x2c, 0x81, 0xad, 0xda, 0xdf, 0x48, 0xba, 0x24, 0x36, 0x0a, 0xf2, 0xfa, 0xb8, 0xb4, 0x64, 0x98, 0xc5, 0xbf, 0xc9, 0xbe, 0xbd, 0x19, 0x8e, 0x26, 0x8c, 0x3b, 0xa7, 0x09, 0xe0, 0x42, 0x14, 0x68, 0x00, 0x7b, 0xac, 0xb2, 0xdf, 0x33, 0x16, 0x96, 0xe9, 0x39, 0xe4, 0x6c, 0x51, 0x8d, 0x80, 0xc8, 0x14, 0xe2, 0x04, 0x76, 0xa9, 0xfb, 0x8a, 0x50, 0x25, 0xc0, 0x2d, 0x59, 0xc5, 0x82, 0x39, 0xde, 0x13, 0x69, 0x67, 0x6c, 0xcc, 0x5a, 0x71, 0xfa, 0x25, 0x63, 0x95, 0x96, 0x74, 0xee, 0x15, 0x58, 0x86, 0xca, 0x5d, 0x2e, 0x2f, 0x31, 0xd7, 0x7e, 0x0a, 0xf1, 0xfa, 0x27, 0xcf, 0x73, 0xc3, 0x74, 0x9c, 0x47, 0xab, 0x18, 0x50, 0x1d, 0xda, 0xe2, 0x75, 0x7e, 0x4f, 0x74, 0x01, 0x90, 0x5a, 0xca, 0xfa, 0xaa, 0xe3, 0xe4, 0xd5, 0x9b, 0x34, 0x9a, 0xdf, 0x6a, 0xce, 0xbd, 0x10, 0x19, 0x0d, 0xfe, 0x48, 0x90, 0xd1, 0xe6, 0x18, 0x8d, 0x0b, 0x04, 0x6d, 0xf3, 0x44, 0x70, 0x6c, 0x63, 0x1e};
+    byte keys[240];
+
+    printf("\nTest AES256 key expansion\n");
+    expand_key256(key, keys);
+    if (memcmp(keys, expected_keys, 240) == 0) {
+        printf("Expanded key matches expected value.\n");
+        return 0;
+    } else {
+        printf("Expanded key does not match expected value!\n");
+        Pretty(expected_keys, 240, "Expected expanded key: ");
+        Pretty(keys, 240, "Actual expanded key:   ");
+        return -8;
+    }
+}
+
+int test_encrypt_256() {
+    // test vector from Appendix C.3 of FIPS 197
+    byte plaintext[] = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff};
+    byte key[] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f};
+    byte expected_ciphertext[] = {0x8e, 0xa2, 0xb7, 0xca, 0x51, 0x67, 0x45, 0xbf, 0xea, 0xfc, 0x49, 0x90, 0x4b, 0x49, 0x60, 0x89};
+    byte ciphertext[16];
+
+    printf("\nTest AES256 encryption\n");
+    EncryptAES256(plaintext, key, ciphertext);
+    if (memcmp(ciphertext, expected_ciphertext, 16) == 0) {
+        printf("Ciphertext matches expected value.\n");
+        return 0;
+    } else {
+        printf("Ciphertext does not match expected value!\n");
+        Pretty(expected_ciphertext, 16, "Expected ciphertext: ");
+        Pretty(ciphertext, 16, "Actual ciphertext:   ");
+        return -16;
+    }
+}
+
+int test_decrypt_256() {
+    // test vector from Appendix C.3 of FIPS 197
+    byte ciphertext[] = {0x8e, 0xa2, 0xb7, 0xca, 0x51, 0x67, 0x45, 0xbf, 0xea, 0xfc, 0x49, 0x90, 0x4b, 0x49, 0x60, 0x89};
+    byte expected_plaintext[] = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff};
+    byte key[] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f};
+    byte plaintext[16];
+
+    printf("\nTest AES256 decryption\n");
+    DecryptAES256(ciphertext, key, plaintext);
+    if (memcmp(plaintext, expected_plaintext, 16) == 0) {
+        printf("Plaintext matches expected value.\n");
+        return 0;
+    } else {
+        printf("Plaintext does not match expected value!\n");
+        Pretty(expected_plaintext, 16, "Expected plaintext: ");
+        Pretty(plaintext, 16, "Actual plaintext:   ");
+        return -32;
+    }
 }
 
 // Test AES
 int main(void) {
-    byte key[] = {0x12,0x34,0x56,0x12,0x34,0x56,0x12,0x34,0x56,0x12,0x34,0x56,0x12,0x34,0x56,0x12};
-    byte msg[] = {0xab,0xcd,0xef,0xab,0xcd,0xef,0xab,0xcd,0xef,0xab,0xcd,0xef,0xab,0xcd,0xef,0xab};
-    byte encrypted[16], decrypted[16];
+    int r = 0;
 
-    printf("Test AES\r\n\n");
-    Pretty(key,16,"Key:         ");
-    Pretty(msg,16,"Original:    ");
+    r |= test_expand_key_128();
+    r |= test_encrypt_128();
+    r |= test_decrypt_128();
 
-    EncryptAES(msg,key,encrypted);
-    printf("Encrypted should be: 85E5A3D7356A61E29A8AFA559AD67102\r\n");
-    Pretty(encrypted,16,"Encrypted:   ");
+    r |= test_expand_key_256();
+    r |= test_encrypt_256();
+    r |= test_decrypt_256();
 
-    DecryptAES(encrypted,key,decrypted);
-    Pretty(decrypted,16,"Decrypted:   ");
-
-    return 0;
+    return r;
 }
